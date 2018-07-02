cc68a45dfeb5d9da9a0d2aca26a048967eaef361
jrrpanix <jrr@panix.com>
2018-07-02 18:09:16 -0400

    added parsing, better loading

--- python/kraken_load.py
+++ python/kraken_load.py
@@ -3,6 +3,7 @@ from requests.exceptions import HTTPError
 import json
 import time
 import os
+import sys
 
 def getTime(t):
     tm = time.gmtime(t)
@@ -45,7 +46,7 @@ def writeTrades(t, trades):
 def getLast(last_file="./last"):
     if os.path.isfile(last_file) is False:
         return 0
-    data = open(last_file))
+    data = open(last_file)
     last = 0
     for line in data:
         test = int(line)
@@ -54,8 +55,8 @@ def getLast(last_file="./last"):
     return last
 
 
-def loadTrades(pair='XXBTZUSD', since=0, stop_date=0)
-    stop_date = stop_date if stop_date == 0 : else getTimeNowNS()
+def loadTrades(pair='XXBTZUSD', since=0, stop_date=0):
+    stop_date = stop_date if stop_date == 0 else getTimeNowNS()
     fault_count = 0;
     last_file ="{}_{}".format("./last", pair)
     since = since if since != 0 else getLast(last_file)
@@ -77,8 +78,7 @@ def loadTrades(pair='XXBTZUSD', since=0, stop_date=0)
                 rk = res[k]
                 firstTime = getTime(rk[0][2])
                 writeTrades(firstTime, trades)
-                t0 getTime(rk[0][2])
-                tN getTime(rk[-1][2])
+                t0, tN = getTime(rk[0][2]), getTime(rk[-1][2])
                 print(t0, tN, len(rk))
 
 def testTrades():
@@ -109,30 +109,41 @@ def testTrades():
 #    with open("./tmp.json", "w") as f :
 #        f.write(json.dumps(trades, indent=4))
 
-def readJson():
-    with open("./tmp.json") as f:
-        j = json.load(f)
-        assert 'result' in j
-        res = j['result']
-        for k in res.keys():
-            rk = res[k]
-            if k == 'last' :
-                print(rk)
-            else:
-                t0 = "NA" if k == 'last' else getTime(rk[0][2])
-                tN = "NA" if k == 'last' else getTime(rk[-1][2])
-
-                print(k, len(rk), rk[0], t0, rk[-1], tN)
-                for r in rk:
-                    print(r, getTime(r[2]))
+def readJson(json_data):
+    if not 'result' in json_data: return None
+    res = j['result']
+    for k in res.keys():
+        rk = res[k]
+        if k == 'last' :
+            continue
+        else:
+            print(k, len(rk), rk[0], t0, rk[-1], tN)
+#            for r in rk:
+#               print(r, getTime(r[2]))
 
 
+def createCSV(data_dir, output_file="./data.csv") :
+    wf = open(output_file, "w")
+    for f in sorted(os.listdir(data_dir)):
+        try:
+            json_data = json.load(open(os.path.join(data_dir, f)))
+            if not 'result' in json_data: continue
+            result_json = json_data['result']
+            pair = None
+            for key in result_json.keys() :
+                if not key == 'last' :
+                    pair = key
+                    break
+            assert pair is not None
+            trade_data = result_json[pair]
+            for r in trade_data:
+                line = "{},{},{},{},{},{}\n".format(r[0], r[1], r[2], r[3],r[4],r[5])
+                wf.write(line)
+        except:
+            print(f)
+                    
 if __name__ == '__main__':
-    #readJson()
-    t = time.time()
-    tm = time.gmtime(t)
-    print(tm)
-    #ts = time.strftime('%m/%d/%Y %H:%M:%S', tm)
-    ts = time.strftime('%Y%m%d_%H%M%S', tm)
-    print(ts)
-    testTrades()
+    #testTrades()
+    assert len(sys.argv) > 1
+    data_dir = sys.argv[1]
+    createCSV(data_dir)
